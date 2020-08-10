08a85f5a14574a717f49634306d600ed0036b4e6
Bonanov <38221096+bonanov@users.noreply.github.com>
2020-08-10 19:14:14 +0300

    sql setups

--- migrations/2020-08-10-092413_setup/up.sql
+++ migrations/2020-08-10-092413_setup/up.sql
@@ -1,20 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE files (
     id BIGSERIAL NOT NULL PRIMARY KEY, 
     hash VARCHAR NOT NULL,
     ext VARCHAR NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE users (
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR NOT NULL,
     nickname VARCHAR,
     email VARCHAR,
+    password VARCHAR,
     color VARCHAR NOT NULL DEFAULT '#eeeeee',
     image_id BIGSERIAL REFERENCES files(id) ON DELETE CASCADE,
     is_admin BOOLEAN NOT NULL DEFAULT 'f',
+    last_login TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE rooms (
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -24,69 +28,76 @@ CREATE TABLE rooms (
     is_deleted BOOLEAN NOT NULL DEFAULT 'f',
     password VARCHAR,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    last_login TIMESTAMP,
     deleted_at TIMESTAMP
-)
+);
+
+CREATE TABLE videos (
+    id BIGSERIAL NOT NULL PRIMARY KEY, 
+    room_id UUID REFERENCES rooms(id) ON DELETE CASCADE,
+    subtitles_id BIGSERIAL REFERENCES files(id) ON DELETE CASCADE,
+    url VARCHAR NOT NULL,
+    title VARCHAR,
+    duration INTEGER NOT NULL DEFAULT -1,
+    is_raw BOOLEAN NOT NULL DEFAULT 'f',
+    is_iframe BOOLEAN NOT NULL DEFAULT 'f',
+    is_live BOOLEAN NOT NULL DEFAULT 'f',
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
 
 CREATE TABLE roles (
     id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     room_id UUID REFERENCES rooms(id) ON DELETE CASCADE,
     name VARCHAR NOT NULL,
     is_default BOOLEAN NOT NULL DEFAULT 'f',
-    position SMALLSERIAL NULL DEFAULT 999,
+    position SMALLINT NOT NULL DEFAULT 999,
 
-    title_update SMALLINT DEFAULT -1,
-    path_update SMALLINT DEFAULT -1,
-    public_update INTEGER DEFAULT -1,
-    room_delete INTEGER DEFAULT -1,
-    audit_log_read SMALLINT DEFAULT -1,
+    title_update SMALLINT NOT NULL DEFAULT -1,
+    path_update SMALLINT NOT NULL DEFAULT -1,
+    public_update INTEGER NOT NULL DEFAULT -1,
+    room_delete INTEGER NOT NULL DEFAULT -1,
+    audit_log_read SMALLINT NOT NULL DEFAULT -1,
 
-    embed_links INTEGER DEFAULT -1,
-    ping_everyone INTEGER DEFAULT -1,
+    embed_links INTEGER NOT NULL DEFAULT -1,
+    ping_everyone INTEGER NOT NULL DEFAULT -1,
 
-    password_create SMALLINT DEFAULT -1,
-    password_update SMALLINT DEFAULT -1,
-    password_delete SMALLINT DEFAULT -1,
+    password_create SMALLINT NOT NULL DEFAULT -1,
+    password_update SMALLINT NOT NULL DEFAULT -1,
+    password_delete SMALLINT NOT NULL DEFAULT -1,
 
-    emote_create SMALLINT DEFAULT -1,
-    emote_update SMALLINT DEFAULT -1,
-    emote_delete SMALLINT DEFAULT -1,
-    emote_view SMALLINT DEFAULT -1,
+    emote_create SMALLINT NOT NULL DEFAULT -1,
+    emote_update SMALLINT NOT NULL DEFAULT -1,
+    emote_delete SMALLINT NOT NULL DEFAULT -1,
+    emote_view SMALLINT NOT NULL DEFAULT -1,
 
-    role_create SMALLINT DEFAULT -1,
-    role_delete SMALLINT DEFAULT -1,
-    role_update SMALLINT DEFAULT -1,
-    role_view SMALLINT DEFAULT -1,
+    role_create SMALLINT NOT NULL DEFAULT -1,
+    role_delete SMALLINT NOT NULL DEFAULT -1,
+    role_update SMALLINT NOT NULL DEFAULT -1,
+    role_view SMALLINT NOT NULL DEFAULT -1,
 
-    video_create SMALLINT DEFAULT -1,
-    video_delete SMALLINT DEFAULT -1,
-    video_watch SMALLINT DEFAULT -1,
-    video_move SMALLINT DEFAULT -1,
-    video_iframe SMALLINT DEFAULT -1,
-    video_raw SMALLINT DEFAULT -1,
+    video_create SMALLINT NOT NULL DEFAULT -1,
+    video_delete SMALLINT NOT NULL DEFAULT -1,
+    video_watch SMALLINT NOT NULL DEFAULT -1,
+    video_move SMALLINT NOT NULL DEFAULT -1,
+    video_iframe SMALLINT NOT NULL DEFAULT -1,
+    video_raw SMALLINT NOT NULL DEFAULT -1,
 
-    player_pause SMALLINT DEFAULT -1,
-    player_resume SMALLINT DEFAULT -1,
-    player_rewind SMALLINT DEFAULT -1,
-    subtitles_file SMALLINT DEFAULT -1,
-    subtitles_embed SMALLINT DEFAULT -1,
+    player_pause SMALLINT NOT NULL DEFAULT -1,
+    player_resume SMALLINT NOT NULL DEFAULT -1,
+    player_rewind SMALLINT NOT NULL DEFAULT -1,
+    subtitles_file SMALLINT NOT NULL DEFAULT -1,
+    subtitles_embed SMALLINT NOT NULL DEFAULT -1,
 
-    message_create SMALLINT DEFAULT -1,
-    message_read SMALLINT DEFAULT -1,
-    message_history_read SMALLINT DEFAULT -1,
-    message_timeout INTEGER DEFAULT -1,
+    message_create SMALLINT NOT NULL DEFAULT -1,
+    message_read SMALLINT NOT NULL DEFAULT -1,
+    message_history_read SMALLINT NOT NULL DEFAULT -1,
+    message_timeout INTEGER NOT NULL DEFAULT -1,
 
-    user_kick SMALLINT DEFAULT -1,
-    user_ban SMALLINT DEFAULT -1,
-    user_unban SMALLINT DEFAULT -1,
-    user_timeout SMALLINT DEFAULT -1
-)
-
--- "Everyone" - 1002
--- "Anonymous" - 1001
--- "Stranger" - 1000
--- "DJ" - 2
--- "Mod" - 1
--- "Owner" - 0
+    user_kick SMALLINT NOT NULL DEFAULT -1,
+    user_ban SMALLINT NOT NULL DEFAULT -1,
+    user_unban SMALLINT NOT NULL DEFAULT -1,
+    user_timeout SMALLINT NOT NULL DEFAULT -1
+);
 
 CREATE TABLE emotes (
     id UUID NOT NULL PRIMARY KEY, 
@@ -97,8 +108,7 @@ CREATE TABLE emotes (
     is_deleted BOOLEAN NOT NULL DEFAULT 'f',
     deleted_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
-
+);
 
 -- Messages
 
@@ -107,37 +117,47 @@ CREATE TABLE channels (
     id UUID NOT NULL PRIMARY KEY,
     deleted_at TIMESTAMP NOT NULL DEFAULT NOW(),
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 -- Channel of type ROOM
 CREATE TABLE room_channels (
     id UUID NOT NULL PRIMARY KEY,
     channels_id UUID REFERENCES channels(id) ON DELETE CASCADE,
-    room_id UUID REFERENCES rooms(id) ON DELETE CASCADE,
-)
+    room_id UUID REFERENCES rooms(id) ON DELETE CASCADE
+);
 -- Channel of type DM
 CREATE TABLE dm_channels (
     id UUID NOT NULL PRIMARY KEY,
-    channels_id UUID REFERENCES channels(id) ON DELETE CASCADE,
-)
+    channels_id UUID REFERENCES channels(id) ON DELETE CASCADE
+);
 
 -- dm participants
 CREATE TABLE dm_channel_users (
     id UUID NOT NULL PRIMARY KEY, 
     user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-    dm_channel_id UUID REFERENCES dm_channels(id) ON DELETE CASCADE,
-)
+    dm_channel_id UUID REFERENCES dm_channels(id) ON DELETE CASCADE
+);
 
 -- message itself
 CREATE TABLE messages (
     id UUID NOT NULL PRIMARY KEY, 
     channel_id UUID REFERENCES channels(id) ON DELETE CASCADE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
     content VARCHAR,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
-CREATE TABLE restrictions (
+-- message itself
+CREATE TABLE message_mentions (
+    id BIGSERIAL NOT NULL PRIMARY KEY, 
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    messages_id UUID REFERENCES messages(id) ON DELETE CASCADE
+);
+
+-- bans and timeouts
+CREATE TABLE restrains (
     id UUID NOT NULL PRIMARY KEY, 
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 
     ip VARCHAR,
     fingerprint VARCHAR,
@@ -147,7 +167,7 @@ CREATE TABLE restrictions (
     is_ban BOOLEAN NOT NULL DEFAULT 'f',
     ending_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE audit_logs (
     id BIGSERIAL NOT NULL PRIMARY KEY, 
@@ -160,4 +180,4 @@ CREATE TABLE audit_logs (
 
     table_name VARCHAR NOT NULL,
     changes VARCHAR NOT NULL
-)
+);
--- src/debug.rs
+++ src/debug.rs
@@ -3,18 +3,17 @@ use env_logger::{Builder, WriteStyle};
 use std::io::Write;
 
 pub fn init() {
-    let mut builder = Builder::from_default_env();
+        let mut builder = Builder::from_default_env();
 
-    builder
-        .format(|buf, record| {
-            writeln!(
-                buf,
-                "{:?}|{}|{}|{}",
-                Utc::now(),
-                record.level(),
-                record.target(),
-                record.args()
-            )
+        builder.format(|buf, record| {
+                writeln!(
+                        buf,
+                        "{:?}|{}|{}|{}",
+                        Utc::now(),
+                        record.level(),
+                        record.target(),
+                        record.args()
+                )
         })
         .write_style(WriteStyle::Always)
         .init();
