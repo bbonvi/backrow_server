3e0619f4c3ad91cc07bcb973455378a609972d4e
Bonanov <38221096+bonanov@users.noreply.github.com>
2020-08-14 11:24:06 +0300

    fixes

--- src/server/asserts.rs
+++ src/server/asserts.rs
@@ -1,6 +1,16 @@
 use crate::env;
-use crate::vars;
+use crate::vars::*;
 use regex::Regex;
+
+fn in_range(string: &str, min: usize, max: usize) -> bool {
+    let char_count = string.chars().count();
+    char_count >= min && char_count <= max
+}
+
+fn matches(string: &str, reg_string: &str) -> bool {
+    let re = Regex::new(reg_string).unwrap();
+    re.is_match(&string)
+}
 
 pub fn valid_origin(req: &actix_web::HttpRequest) -> bool {
     let app_origin = env::APP_ORIGIN.clone();
@@ -9,30 +19,29 @@ pub fn valid_origin(req: &actix_web::HttpRequest) -> bool {
 }
 
 pub fn valid_username(username: &String) -> bool {
-    let char_count = username.chars().count();
-    if char_count < vars::USERNAME_MIN_LEN || char_count > vars::USERNAME_MAX_LEN {
-        return false;
-    }
-
-    let re = Regex::new(r"^[a-zA-Z0-9_]+$").unwrap();
-    re.is_match(&username)
+    in_range(username, USERNAME_MIN_LEN, USERNAME_MAX_LEN) && matches("^[a-zA-Z0-9_]+$", &username)
 }
-pub fn valid_password(username: &String) -> bool {
-    let char_count = username.chars().count();
-    char_count >= vars::PASSWORD_MIN_LEN && char_count <= vars::PASSWORD_MAX_LEN
+
+pub fn valid_password(password: &String) -> bool {
+    in_range(password, PASSWORD_MIN_LEN, PASSWORD_MAX_LEN)
 }
 
 pub fn valid_nickname(nickname: String) -> bool {
-    let char_count = nickname.chars().count();
-    char_count >= vars::NICKNAME_MIN_LEN && char_count <= vars::NICKNAME_MAX_LEN
+    in_range(&nickname, NICKNAME_MIN_LEN, NICKNAME_MAX_LEN)
 }
 
 pub fn valid_email(email: String) -> bool {
-    let char_count = email.chars().count();
-    if char_count < 4 || char_count > 40 {
-        return false;
-    }
+    in_range(&email, 4, 100)
+        && matches(
+            &email,
+            &r#"^[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~."\(\),:;<>@\[\]]+$""#,
+        )
+}
 
-    let re = Regex::new(r#"^[a-zA-Z0-9!#$%&'*+-/=?^_`{|}~."\(\),:;<>@\[\]]+$""#).unwrap();
-    re.is_match(&email)
+pub fn valid_room_name(name: String) -> bool {
+    in_range(&name, ROOM_NAME_MIN_LEN, ROOM_NAME_MAX_LEN)
+}
+
+pub fn valid_room_path(username: &String) -> bool {
+    in_range(username, USERNAME_MIN_LEN, USERNAME_MAX_LEN) && matches(username, r"^[a-zA-Z0-9_]+$")
 }
